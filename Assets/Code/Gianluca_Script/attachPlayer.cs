4862db10dbfd7af400d34d151d4cc9833368c566
AACTheCrow <gianluca.valli.2@gmail.com>
2025-04-06 13:31:07 +0200

    Fixed Project Setting & Camera Trigger

---
+++ Assets/Code/Gianluca_Script/attachPlayer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class attachPlayer : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.SetParent(this.transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.SetParent(null);
+        }
+    }
+}
